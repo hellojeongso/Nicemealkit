5a55a119d546a55d73f95ef796dfa4b61cb1a5f9
hellojeongso <84715469+hellojeongso@users.noreply.github.com>
2023-05-16 05:22:36 +0900

    HTML, CSS, JS: category

--- js/custom.js
+++ js/custom.js
@@ -2,4 +2,16 @@ $(function(){
   // include html Files
   $('.gnb-include').load('/include/gnb.html')
   $('.main-header-include').load('/include/main-header.html')
+
+  // category Accordion
+  $('.category-accordion .detail').eq(0).show()
+  $('.category-accordion .title').click(function(){
+    // $(this).next().stop().slideToggle()
+    $(this).next().stop().slideDown()
+    $(this).siblings('.category-accordion .title').next().stop().slideUp()
+    
+    $(this).addClass('active')
+    $(this).siblings('.category-accordion .title').
+    removeClass('active')
+  })
 })
