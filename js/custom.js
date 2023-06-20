db69b8837e92acda8dfe5ad358750d07968d4e2e
hellojeongso <84715469+hellojeongso@users.noreply.github.com>
2023-06-20 16:01:39 +0900

    HTML, CSS, JS: detail

--- js/custom.js
+++ js/custom.js
@@ -2,6 +2,7 @@ $(function(){
   // include html Files
   $('.gnb-include').load('/include/gnb.html')
   $('.main-header-include').load('/include/main-header.html')
+  $('.goods-detail-header-include').load('/include/goods-detail-header.html')
 
   // category Accordion
   $('.category-accordion .detail').eq(0).show()
@@ -62,4 +63,36 @@ $(function(){
   $('.btn-view-more').click(function(){
     $('.company-info-more').show()
   })
+
+  // Goods Detail Slider
+  $('.goods-detail-slider').slick({
+    slidesToShow: 1,
+    dots: true,
+    arrows: false,
+    slidesToScroll: 1
+  })
+
+  // Goods Accordion
+  $('.goods-accordion .detail').eq(0).show()
+
+  $('.goods-accordion .title').click(function(){
+    $(this).next().toggle()
+    $(this).addClass('active')
+    $(this).siblings('.goods-accordion .title').removeClass('active')
+  })  // siblings에서도 어떤건지 명시해줘야함
+
+  // Goods Order Info
+  $('.btn-order-choice').click(function(){
+    $('.goods-order-choice').hide()
+    $('.goods-order-final').show()
+    $('.overlay-fold').show()
+  })
+
+  $('.btn-fold, .overlay-fold').click(function(){
+    $('.goods-order-choice').show()
+    $('.goods-order-final').hide()
+    $('.overlay-fold').hide()
+  })
+
+
 })
